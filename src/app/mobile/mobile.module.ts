6a8f6597d76234b377fce87e14310cf31d23e090
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-15 00:20:26 +0530

    update mobile-routing.module.ts, mobile.component.html and mobile.module.ts

--- src/app/mobile/mobile.module.ts
+++ src/app/mobile/mobile.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MobileRoutingModule } from './mobile-routing.module';
+import { MobileComponent } from './mobile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MobileComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MobileRoutingModule
   ]
 })
 export class MobileModule { }
