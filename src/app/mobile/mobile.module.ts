b557956c357f2093b01574e827d5cbb68da30fde
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-15 04:06:05 +0530

    update mobile.module.ts

--- src/app/mobile/mobile.module.ts
+++ src/app/mobile/mobile.module.ts
@@ -4,12 +4,25 @@ import { MobileRoutingModule } from './mobile-routing.module';
 import { MobileComponent } from './mobile.component';
 import { HeaderComponent } from "./components/layout/header/header.component";
 import { FooterComponent } from "./components/layout/footer/footer.component";
+import { UserComponent } from './pages/user/user.component';
+import { DirectComponent } from './pages/direct/direct.component';
+import { ExploreComponent } from './pages/explore/explore.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InboxComponent } from './pages/inbox/inbox.component';
+import { ReelsComponent } from './pages/reels/reels.component';
 
 @NgModule({
   declarations: [
     MobileComponent,
     HeaderComponent,
     FooterComponent,
+    UserComponent,
+    DirectComponent,
+    ExploreComponent,
+    HomeComponent,
+    InboxComponent,
+    ReelsComponent
+
   ],
   imports: [
     CommonModule,
