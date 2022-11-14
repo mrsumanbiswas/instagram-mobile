7572d64684b0646fd1c9aa3fcdbd8f2576a43dc7
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-14 21:28:59 +0530

    update environment.prod.ts and environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'XXXXXXXXXXXXXXXXXXXX',
+    appId: '1:XXXXXXXXXXXXX:XXX:XXXXXXXXXXXXXXXXXXXXXX',
+    storageBucket: 'XXXXXXXXXXXXXXXXXXXX.XXXXXX.XXX',
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXXXXXXXXXXX.XXXXXXXXX.XXXX',
+    messagingSenderId: 'XXXXXXXXXXXXX',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'XXXXXXXXXXXXXXXXXXXX',
+    appId: '1:XXXXXXXXXXXXX:XXX:XXXXXXXXXXXXXXXXXXXXXX',
+    storageBucket: 'XXXXXXXXXXXXXXXXXXXX.XXXXXX.XXX',
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXXXXXXXXXXX.XXXXXXXXX.XXXX',
+    messagingSenderId: 'XXXXXXXXXXXXX',
+  },
   production: false
 };
 
