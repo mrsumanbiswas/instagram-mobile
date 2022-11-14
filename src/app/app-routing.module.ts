91c99ebdd6c78e4baac86f112641682054d87983
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-15 00:33:52 +0530

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MobileComponent } from './mobile/mobile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'m',
+  component:MobileComponent,
+  loadChildren:()=>import('./mobile/mobile.module').then(m =>m.MobileModule),
+},
+{
+  path:'**',
+  redirectTo:'m',
+  pathMatch:'full',
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
