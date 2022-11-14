892c77678f8dd6a3caa4355eb7dab1799197898c
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-15 00:33:41 +0530

    update mobile-routing.module.ts

--- src/app/mobile/mobile-routing.module.ts
+++ src/app/mobile/mobile-routing.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import{ ExploreComponent } from './pages/explore/explore.component'
+import { ReelsComponent } from './pages/reels/reels.component';
+import { InboxComponent } from './pages/inbox/inbox.component';
+import { DirectComponent } from './pages/direct/direct.component';
 
 const routes: Routes = [
   {
     path:'home',
     component:HomeComponent
   },
+  {
+    path:'direct',
+    component:DirectComponent
+  },
+  {
+    path:'explore',
+    component:ExploreComponent
+  },
+  {
+    path:'reels',
+    component:ReelsComponent
+  },
+  {
+    path:'inbox',
+    component:InboxComponent
+  },
+  
   {
     path:'',
     redirectTo:'home',
