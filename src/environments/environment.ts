55caebe7388c33243dc41da8b86251133f9e25b1
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-14 21:37:23 +0530

    update .gitignore, environment.prod.ts and environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   firebase: {
-    projectId: 'XXXXXXXXXXXXXXXXXXXX',
-    appId: '1:XXXXXXXXXXXXX:XXX:XXXXXXXXXXXXXXXXXXXXXX',
-    storageBucket: 'XXXXXXXXXXXXXXXXXXXX.XXXXXX.XXX',
-    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
-    authDomain: 'XXXXXXXXXXXXXXXXXXXX.XXXXXXXXX.XXXX',
-    messagingSenderId: 'XXXXXXXXXXXXX',
+    projectId: 'instagramwebappclone',
+    appId: '1:582745665784:web:cf4512b4a57a56251d4b0d',
+    storageBucket: 'instagramwebappclone.appspot.com',
+    apiKey: 'AIzaSyBkyTWp_Iv31Q2a9ujUgLqnFLbgq5DRWGw',
+    authDomain: 'instagramwebappclone.firebaseapp.com',
+    messagingSenderId: '582745665784',
   },
   production: false
 };
