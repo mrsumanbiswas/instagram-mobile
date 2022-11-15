2fc0709f329e34ed198076ddeaca878af672a558
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-15 06:59:48 +0530

    update posts.component.html, posts.component.scss and posts.component.ts

--- src/app/mobile/components/posts/posts.component.ts
+++ src/app/mobile/components/posts/posts.component.ts
@@ -6,6 +6,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit {
+  posts =[
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    
+  ];
+
 
   constructor() { }
 
