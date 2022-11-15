7d4c9abb57894f9898b6f9f02154bee5fb27ca8e
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-15 09:00:21 +0530

    update feed.component.ts

--- src/app/mobile/components/feed/feed.component.ts
+++ src/app/mobile/components/feed/feed.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-feed',
+  selector: 'mobile-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss']
 })
