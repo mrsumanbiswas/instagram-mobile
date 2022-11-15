5f9acb78c37e2f8ea8122c5d52167e77b5a31740
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-15 09:00:04 +0530

    create suggetion.component.html, suggetion.component.scss, suggetion.component.spec.ts and suggetion.component.ts

---
+++ src/app/mobile/components/suggetion/suggetion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mobile-suggetion',
+  templateUrl: './suggetion.component.html',
+  styleUrls: ['./suggetion.component.scss']
+})
+export class SuggetionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
