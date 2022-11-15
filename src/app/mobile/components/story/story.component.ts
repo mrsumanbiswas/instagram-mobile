1f72bdb2583c7052a751656976beabd4853834aa
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-15 08:59:57 +0530

    create story.component.html, story.component.scss, story.component.spec.ts and story.component.ts

---
+++ src/app/mobile/components/story/story.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mobile-story',
+  templateUrl: './story.component.html',
+  styleUrls: ['./story.component.scss']
+})
+export class StoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
