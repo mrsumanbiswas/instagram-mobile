efa4a780cd90bfc95473cdc73516ae9f1a4eb49b
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-17 03:17:09 +0530

    create reels.component.html, reels.component.scss, reels.component.spec.ts and reels.component.ts

---
+++ v2/src/app/mobile/pages/reels/reels.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './reels.component.html',
+  styleUrls: ['./reels.component.scss']
+})
+export class ReelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
