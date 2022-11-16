6f8eb01caa5bcf606582ab7460c5c6498684bf3d
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-17 03:50:06 +0530

    chore: rename footer.component.html, footer.component.scss, footer.component.spec.ts and footer.component.ts

--- v2/src/app/mobile/components/footer/footer.component.ts
+++ v2/src/app/mobile/components/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  selector:'mobile-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
