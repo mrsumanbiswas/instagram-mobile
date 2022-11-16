a8802bc598bacf5efce690adaf9af652592d5dfa
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-17 03:20:57 +0530

    create followers.component.html, followers.component.scss, followers.component.spec.ts and followers.component.ts

---
+++ v2/src/app/mobile/components/followers/followers.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './followers.component.html',
+  styleUrls: ['./followers.component.scss']
+})
+export class FollowersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
