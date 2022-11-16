e88fbbec2f846bb030251dd2a78dc950239c3a94
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-17 03:41:34 +0530

    create 2 files and update 7 files #firebase

--- v2/src/app/app.module.ts
+++ v2/src/app/app.module.ts
@@ -5,6 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MobileComponent } from './mobile/mobile.component';
 import { WebComponent } from './web/web.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -14,9 +24,20 @@ import { WebComponent } from './web/web.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- v2/src/environments/environment.ts
+++ v2/src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'instagramwebappclone',
+    appId: '1:582745665784:web:cf4512b4a57a56251d4b0d',
+    storageBucket: 'instagramwebappclone.appspot.com',
+    apiKey: 'AIzaSyBkyTWp_Iv31Q2a9ujUgLqnFLbgq5DRWGw',
+    authDomain: 'instagramwebappclone.firebaseapp.com',
+    messagingSenderId: '582745665784',
+  },
   production: false
 };
 
