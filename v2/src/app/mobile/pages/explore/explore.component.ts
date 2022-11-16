c3cbb14b3b49c5b970b035717d614138c3d93302
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-17 03:17:21 +0530

    create explore.component.html, explore.component.scss, explore.component.spec.ts and explore.component.ts

---
+++ v2/src/app/mobile/pages/explore/explore.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './explore.component.html',
+  styleUrls: ['./explore.component.scss']
+})
+export class ExploreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
