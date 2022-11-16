ffb6582b93a9ddf642a993d8ce5e563140c11d08
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-17 03:10:06 +0530

    update 1 file and create 4 files

--- v2/src/app/app.module.ts
+++ v2/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MobileComponent } from './mobile/mobile.component';
+import { WebComponent } from './web/web.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MobileComponent,
+    WebComponent
   ],
   imports: [
     BrowserModule,
