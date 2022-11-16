035caccbed201f4bbdf2842cf8dbcda281f7473a
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-17 03:16:42 +0530

    create direct.component.html, direct.component.scss, direct.component.spec.ts and direct.component.ts

---
+++ v2/src/app/mobile/pages/direct/direct.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './direct.component.html',
+  styleUrls: ['./direct.component.scss']
+})
+export class DirectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
