de61c152cc38aa2b8c2f712d7b3298e34bd8eeb4
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-19 12:07:32 +0530

    update app.module.ts

--- v1/src/app/app.module.ts
+++ v1/src/app/app.module.ts
@@ -15,10 +15,40 @@ import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config
 import { provideStorage,getStorage } from '@angular/fire/storage';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { DirectComponent } from './pages/direct/direct.component';
+import { ExploreComponent } from './pages/explore/explore.component';
+import { InboxComponent } from './pages/inbox/inbox.component';
+import { ReelsComponent } from './pages/reels/reels.component';
+import { UserComponent } from './pages/user/user.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { FollowersComponent } from './components/followers/followers.component';
+import { FollowingComponent } from './components/following/following.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { SavedComponent } from './components/saved/saved.component';
+import { StoryComponent } from './components/story/story.component';
+import { SuggetionComponent } from './components/suggetion/suggetion.component';
+import { TaggedComponent } from './components/tagged/tagged.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DirectComponent,
+    ExploreComponent,
+    InboxComponent,
+    ReelsComponent,
+    UserComponent,
+    FeedComponent,
+    FollowersComponent,
+    FollowingComponent,
+    PostsComponent,
+    SavedComponent,
+    StoryComponent,
+    SuggetionComponent,
+    TaggedComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
