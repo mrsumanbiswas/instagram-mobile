12f89b4fedc745a5cb9cf359bf0c60e90abda4ae
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-19 12:07:26 +0530

    create tagged.component.html, tagged.component.scss, tagged.component.spec.ts and tagged.component.ts

---
+++ v1/src/app/components/tagged/tagged.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tagged',
+  templateUrl: './tagged.component.html',
+  styleUrls: ['./tagged.component.scss']
+})
+export class TaggedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
