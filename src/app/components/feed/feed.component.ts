7fbf3e4276f958922a903b520f312e0118637405
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-19 12:25:18 +0530

    update feed.component.ts

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -7,6 +7,106 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  feeds = [
+    {
+      user: {
+        userid: "userid",
+        avatar: {
+          src: "/assets/icons/icon-72x72.png",
+          alt: "usar-avatar"
+        }
+      },
+      content: {
+        url: {
+          src: "/assets/icons/icon-72x72.png",
+          alt: "aa"
+        },
+        desc: "123456789011121314"
+      },
+      details: {
+        like: {
+          count: 0,
+          users: []
+        },
+        comment: {
+          count: 0,
+          comments: [
+            {
+              user: "",
+              comment: ""
+            }
+          ]
+        },
+        timestamp: Date.now()
+      }
+    },
+    {
+      user: {
+        userid: "userid",
+        avatar: {
+          src: "/assets/icons/icon-72x72.png",
+          alt: "usar-avatar"
+        }
+      },
+      content: {
+        url: {
+          src: "/assets/icons/icon-72x72.png",
+          alt: "aa"
+        },
+        desc: "123456789011121314"
+      },
+      details: {
+        like: {
+          count: 0,
+          users: []
+        },
+        comment: {
+          count: 0,
+          comments: [
+            {
+              user: "",
+              comment: ""
+            }
+          ]
+        },
+        timestamp: Date.now()
+      }
+    },
+    {
+      user: {
+        userid: "userid",
+        avatar: {
+          src: "/assets/icons/icon-72x72.png",
+          alt: "usar-avatar"
+        }
+      },
+      content: {
+        url: {
+          src: "/assets/icons/icon-72x72.png",
+          alt: "aa"
+        },
+        desc: "123456789011121314"
+      },
+      details: {
+        like: {
+          count: 0,
+          users: []
+        },
+        comment: {
+          count: 0,
+          comments: [
+            {
+              user: "",
+              comment: ""
+            }
+          ]
+        },
+        timestamp: Date.now()
+      }
+    }
+  ];
+
+  
   constructor() { }
 
   ngOnInit(): void {
