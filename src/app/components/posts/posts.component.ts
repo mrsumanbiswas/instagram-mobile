e7dfb8e0134b483aa711c7324e30ba14a6f44e4f
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-19 12:27:39 +0530

    update posts.component.ts

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -7,6 +7,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
+  posts =[
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    {
+      src:"/assets/icons/icon-144x144.png",
+      alt:"/assets/icons/icon-144x144.png",
+      url:"/assets/icons/icon-144x144.png"
+    },
+    
+  ];
+
+
   constructor() { }
 
   ngOnInit(): void {
