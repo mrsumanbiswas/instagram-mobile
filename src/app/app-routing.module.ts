43bafc1be06b1577c0df6930256c272ef6d74848
Suman Biswas <mrsumanbiswas0@gmail.com>
2022-11-19 12:19:50 +0530

    update 3 files and delete 4 files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,79 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MobileComponent } from './mobile/mobile.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { FollowersComponent } from './components/followers/followers.component';
+import { FollowingComponent } from './components/following/following.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { SavedComponent } from './components/saved/saved.component';
+import { TaggedComponent } from './components/tagged/tagged.component';
+import { DirectComponent } from './pages/direct/direct.component';
+import { ExploreComponent } from './pages/explore/explore.component';
+import { HomeComponent } from './pages/home/home.component';
+import { InboxComponent } from './pages/inbox/inbox.component';
+import { ReelsComponent } from './pages/reels/reels.component';
+import { UserComponent } from './pages/user/user.component';
 
 const routes: Routes = [
-{
-  path:'',
-  component:MobileComponent,
-  loadChildren:()=>import('./mobile/mobile.module').then(m =>m.MobileModule),
-},
-{
-  path:'**',
-  redirectTo:'',
-  pathMatch:'full',
-}
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'direct',
+    component:DirectComponent
+  },
+  {
+    path:'explore',
+    component:ExploreComponent
+  },
+  {
+    path:'reels',
+    component:ReelsComponent
+  },
+  {
+    path:'inbox',
+    component:InboxComponent
+  },
+  {
+    path:':user',
+    component:UserComponent,
+    children:[
+      {
+        path:'',
+        component:PostsComponent
+      },
+      {
+        path:'feed',
+        component:FeedComponent
+      },
+      {
+        path:'saved',
+        component:SavedComponent
+      },
+      {
+        path:'tagged',
+        component:TaggedComponent
+      },
+      {
+        path:'followers',
+        component:FollowersComponent
+      },
+      {
+        path:'following',
+        component:FollowingComponent
+      },
+      {
+        path:'**',
+        redirectTo:'',
+        pathMatch:'full'
+      }
+    ]
+  },
+  {
+    path:'**',
+    redirectTo:'home',
+    pathMatch:'full'
+  }
 ];
 
 @NgModule({
